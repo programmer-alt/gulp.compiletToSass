f6750574aefa3eb670927fe80c303f91b4ebe66b
programmer-alt <modstils88@mail.ru>
2023-09-11 12:10:02 +0400

    add inject compile ts in js serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
-  const gulp = require('gulp');
+  const gulp = require ('gulp');
   const sass = require ('gulp-sass')(require('sass'));
   const browserSync = require('browser-sync').create();
   const inject = require ('gulp-inject');
-
+  const ts = require ('gulp-typescript');
+  const tsProject = ts.createProject('tsconfig.json')
   gulp.task('sass', function () {
     return gulp.src('C:/Users/User/Desktop/Project/LessonSaas/saas/style.sass')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('C:/Users/User/Desktop/Project/LessonSaas/css/'))
       .pipe(browserSync.reload({stream:true}))
     });
+  gulp.task('typescript', function () {
+      return gulp.src('C:/Users/User/Desktop/Project/LessonSaas/ts/**/*.ts')
+      .pipe (tsProject())
+      .pipe(gulp.dest('C:/Users/User/Desktop/Project/LessonSaas/js/'))
+      .pipe(browserSync.reload({stream:true}))
+    })
  
     gulp.task('serve', function () {
       browserSync.init({
@@ -21,13 +28,16 @@
       });
     gulp.watch('C:/Users/User/Desktop/Project/LessonSaas/saas/style.sass', gulp.series('sass'))
    gulp.watch('C:/Users/User/Desktop/Project/LessonSaas/index.html').on('change', browserSync.reload);
+   gulp.watch('C:/Users/User/Desktop/Project/LessonSaas/**/*.ts', gulp.series('typescript'));
   });
-  gulp.task('inject', function () {
-    return gulp.src('C:/Users/User/Desktop/Project/LessonSaas/index.html')
-      .pipe(inject(gulp.src('./css/style.css'), {
-        relative: true
-      }))
-      .pipe(gulp.dest('C:/Users/User/Desktop/Project/LessonSaas/'));
+  gulp.task('inject', function (cb) {
+    const target = gulp.src('C:/Users/User/Desktop/Project/LessonSaas/index.html')
+    const sourses = gulp.src(['C:/Users/User/Desktop/Project/LessonSaas/css/style.css', 'C:/Users/User/Desktop/Project/LessonSaas/js/**/*.js'],{read:false});
+    return target.pipe(inject(sourses))
+    .pipe(gulp.dest('C:/Users/User/Desktop/Project/LessonSaas/'))
+      .pipe(browserSync.reload({stream:true}))
+      .on('end', function () {console.log('injection completed'); 
+    cb() });
   });
 
-  gulp.task('default', gulp.series('sass', 'serve','inject')); 
+  gulp.task('default', gulp.series('sass','typescript','inject','serve')); 
